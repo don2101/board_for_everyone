22f29ce4ba03327a1654260f96257d66e4b474d4
염희돈 <don210198@gmail.com>
2019-06-27 22:51:56 +0900

    Posts 모델 생성
    writer, content, created_at, updated_at 필드 추가

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Posts(models.Model):
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
