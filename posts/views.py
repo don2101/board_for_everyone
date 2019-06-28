fd426a681c99d9c7db68147d8c89a040deea076b
염희돈 <don210198@gmail.com>
2019-06-29 01:57:20 +0900

    posts 출력 기능 구현
    - main페이지에서 모든 post를 불러와 출력

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
+def main_page(request):
+    # 모든 posts 출력
+    posts = Posts.objects.all()
+
+    return render(request, 'posts/main.html', {'posts': posts})
