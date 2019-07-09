a1e1388163ecb421fc010b392660b92e58b3bb61
염희돈 <don210198@gmail.com>
2019-07-09 22:17:12 +0900

    RESTful posts 어플리케이션 구축
    - posts 어플리케이션에서 RESTful한 post객체를 반환하는 기능 추가
    
    1. serializers.py
    Posts모델 직렬화 기능을 담당하는 PostsSerializer정의
    
    2. views.py
    main페이지에 접근시 posts객체를 반환
    post페이지 접근시 POST방식을 통해 posts를 업로드하는 기능 구현

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,3 +6,4 @@ class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
         fields = ['writer', 'title', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
--- posts/views.py
+++ posts/views.py
@@ -2,26 +2,34 @@ from django.shortcuts import render, redirect
 from .models import Posts
 from .forms import PostsModelForm
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import PostsSerializer
+
+
 # Create your views here.
+@api_view(['GET'])
 def main_page(request):
     # 모든 posts 출력
     posts = Posts.objects.all()
+    serializer = PostsSerializer(posts, many=True)
 
-    return render(request, 'posts/main.html', {'posts': posts})
+    return Response(serializer.data)
 
 
+@api_view(['GET', 'POST'])
 def create_post(request):
     if request.method == "POST":
-        forms = PostsModelForm(request.POST)
-
-        if forms.is_valid():
-            created_post = forms.save(commit=False)
-            created_post.writer = request.user
-            created_post.save()
+        serializer = PostsSerializer(data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save(writer=request.user)
             
-            return redirect('posts:main')
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     else:
         # modelform 제공
-        forms = PostsModelForm()
-        
-        return render(request, 'posts/post.html', {'forms': forms})
+
+        return Response()
