e347df93cbf7afd8349e247d05cd544d7dc59320
염희돈 <don210198@gmail.com>
2019-08-20 22:26:53 +0900

    회원가입 로직 설정
    - serializer.py에서 입력받을 필드와 serializer 정의
    - urls.py에 토큰 인증, 갱신 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 app_name = "accounts"
 
@@ -7,5 +8,9 @@ urlpatterns = [
     path('signup/', views.create_user, name="signup"),
     path('signin/', views.sign_in, name="signin"),
     path('signout/', views.sign_out, name="signout"),
+    path('token/', obtain_jwt_token, name="token_obtain"),
+    path('token/verify/', verify_jwt_token, name="token_verity"),
+    path('token/refresh/', refresh_jwt_token, name="token_refresh"),
+    
     path('<str:user_name>/', views.mypage, name="mypage"),
 ]
