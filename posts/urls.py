25f3d0bf505a41b376aa88f3c7156c08c12efdee
염희돈 <don210198@gmail.com>
2019-08-21 23:57:46 +0900

    post app에 있는 view를 CBV로 변경
    - PostView, PostDetailView class를 정의하여 구성

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path('', views.main_page, name="main"),
-    path('post/', views.post, name="post"),
-    path('post/<int:post_id>', views.post_deatil, name="post_detail"),
+    path('', views.PostView.as_view(), name="main"),
+    path('post/<int:post_id>/', views.PostDetailView.as_view(), name="post_detail"),
 ]
