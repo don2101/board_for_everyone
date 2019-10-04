ee234317c430af3442360daf45eee5730e354abc
염희돈 <don210198@gmail.com>
2019-10-04 19:23:27 +0900

    test 모듈 추가
    - login, posting에 대한 test 모듈 추가
    - django에서 제공하는 TestCase사용하여 구현

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+import requests
 
 # Create your tests here.
+
+class FunctionTest(TestCase):
+    def setUp(self):
+        self.LOGIN_URL = "http://localhost:8000/accounts/signin/"
+        self.login_data = {
+            "username": "aaa",
+            "password": "qwe",
+        }
+
+        self.POST_URL = "http://localhost:8000/"
+        self.post_body = {
+            "title": "goodmanwfewfwe",
+            "content": "contentwefewf",
+        }
+
+    def test_login(self):
+        result = requests.post(self.LOGIN_URL, data=self.login_data)
+
+        self.post_body["jwt"] = result.text
+        self.assertEqual(result, result)
+
+    def test_post(self):
+        result = requests.post(self.POST_URL, data=self.post_body)
+
+        print(result)
+        self.assertEqual(result, result)
+
